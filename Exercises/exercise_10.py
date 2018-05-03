ab9433a7d33f5346a876b1d1e4810565b68a9a40
sanchit <sanchit@localhost.localdomain>
2018-05-03 13:23:04 -0700

    exercise 10 with break

--- Exercises/exercise_10.py
+++ Exercises/exercise_10.py
@@ -1,137 +1,94 @@
 """
-Exercise 10 - Generators
+Exercise 9
 
-For this exercise you will be writing a class for several different generator functions.
 
-1) Write a class called "Gens".
-    - This class is initialized with a single integer that is called "start"
-    - Include a __str__() method so that when an instance of your class is printed, the returned string includes the value of "start"
-        EX: "Start value for generators class is: 5"
-    - All generator methods should start at the "start" value, if one is not provided, the class should default to a start value of 1
+1) Write a decorator function that prints the:
+     - real world time taken to run the function,
+     - process time used to run the function, and
+     - size of the return value (using sys.getsizeof())
 
-2) Include in this class, the following methods:
-    doubles() - yields number * 2 to infinity, starting at self.start
-        Gens(1).doubles() -> 1, 2, 4, 8, 16, ...
+2) Apply this decorator to the following functions:
+    for_loop() - Create an empty list and append the values 1 to 1,000,000 to the list using a for loop
+    list_comp() - Use list comprehension to create a list of all values 1 to 1,000,000
+    numpy_list() - Create a numpy array with all values 1 to 1,000,000
+    pandas_list() - Create a pandas data frame with all values 1 to 1,000,000
+    generator_list() - Use generator comprehension to create a generator of the values 1 to 1,000,000
+                (generator comprehension is the same as list comprehension, but uses () instead of [])
 
-    fib() - Yields the next number in the fibonacci sequence to infinity, starting at 1
-        Gens(100).fib() -> 1, 1, 2, 3, 5, 8, ...
+3) For each function in #2, write a new function that produces the log10 of every number from 1 to 1,000,000.
+    for_loop_log()
+    list_com_log()
+    numpy_list_log()
+    pandas_list_log()
+    generator_list_log()
 
-    linear(n) - yields number + n to infinity, starting at self.start
-        Gens(1).linear(2) -> 1, 3, 5, 7, 9, ...
-
-    exponential(n) - yields number raised to the power n to infinity, starting at self.start
-        Gens(2).exponential(2) -> 2, 4, 16, 256, ...
-
-    sequence(list) - Ignores starting number, yields one value at a time in the list, looping infinitely many times
-        Gens(0).sequence([2, 3, 4]) -> 2, 3, 4, 2, 3, 4, ...
-
-    triple_half() -  Yields a number * 3, then the number / 2, repeating to infinity, starting at self.start
-        Gens(2).triple_half() -> 2, 6, 3, 9, 4.5, 13.5, ...
-
+There are many different ways to complete this assignment and there is not one single best way that I would prefer.
+The purpose of this exercise is to practice implementing a decorator function and gain experience and knowlege of
+several different modules. As long as your submission does not circumvent the purpose of this exercise and completes
+tasks 1, 2 and 3, then you will receive full credit.
 """
+
+import sys
+import numpy
+import pandas
+import time
 import math
 
-class Gens:
-    """
-    Generator class Gens is to include various functions
-        *doubles()
-        *fib()
-        *linear(n)
-        *exponential(n)
-        *sequence(list)
-        *triple_half()
-    """
-    def __init__(self,start):
-        """
-        Takes one arguement, i.e. single integer that is called "start"
-        """
-        if start is None or start == 0:
-            self.start = 1
-        else:
-            self.start = start
+def time_decorator(my_def):
+    def internal_wrapper():
+        t0 = time.time()
+        def_result = my_def()
+        t1 = time.time()
 
-    def __str__(self):
-        return "Start value for generators class is: {}".format(self.start)
+        print("'{}' finished in {} seconds".format(my_def.__name__, t1-t0))
+        print("'{}' process time in {} seconds".format(my_def.__name__, time.process_time()))
+        return "The size of return is {} ".format(sys.getsizeof(def_result))
+    return internal_wrapper()
 
-    def doubles(self):
-        num = self.start
-        while True:
-            yield num
-            num *= 2
+def for_loop():
+    result_list =[]
+    for i in range(1000000):
+        result_list.append(i+1)
+    return result_list
 
-    
-    def fib(self):
-        num_first = 1
-        num_second = 1
-        yield num_first
-        while True:
-            num_first, num_second = num_second, num_first + num_second
-            yield num_first
+def list_comp():
+    return [i+1 for i in range(1000000)]
 
+def numpy_list():
+    return numpy.arange(1,1000001)
 
-    def linear(self,n):
-        num = self.start
-        while True:
-            yield num
-            num += n
+def pandas_list():
+    return pandas.Series(range(1,1000001))
 
+def generator_list():
+    return (i+1 for i in range(1000000))
 
-    def exponential(self,n):
-        num = self.start
-        while True:
-            yield num
-            num = math.pow(num,n)
+# print(time_decorator(for_loop))
+# print(time_decorator(list_comp))
+# print(time_decorator(numpy_list))
+# print(time_decorator(pandas_list))
+# print(time_decorator(generator_list))
 
+def for_loop_log():
+    result_list =[]
+    for i in range(1000000):
+        result_list.append(math.log(i+1,10))
+    return result_list
 
-    def sequence(self,list):
-        i= 0
-        while True:
-            for num in list:
-                yield num
-                i += 1
+def list_com_log():
+    return [math.log(i+1,10) for i in range(1000000)]
 
+def numpy_list_log():
+    return numpy.log10(numpy.arange(1,1000001))
 
-    def triple_half(self):
-        num = self.start
-        yield num
-        while True:
-            num *= 3
-            yield num
-            num /= 2
-            yield num
+def pandas_list_log():
+    return pandas.Series(range(1,1000001)).apply(math.log10)
 
-            
+def generator_list_log():
+    return (math.log(i+1,10) for i in range(1000000))
 
-def main():
-    obj = Gens(5)
-
-    print("doubles")
-    for num in obj.doubles():
-        print(num)
-
-    print("fib")
-    for num in obj.fib():
-        print(num)
-    
-    print("linear")
-    for num in obj.linear(3):
-        print(num)
-
-    print("exponential")
-    for num in obj.exponential(3):
-        print(num)
-    
-    print("sequence")
-    for num in obj.sequence([3,4,5]):
-        print(num)
-
-    print("triple_half")    
-    for num in obj.triple_half():
-       print(num)
-
-if __name__ == '__main__':
-    main()
-
-    
-
-        
+# print(time_decorator(for_loop_log))
+# print(time_decorator(list_com_log))
+# print(time_decorator(numpy_list_log))
+# print(time_decorator(pandas_list_log))
+# print(time_decorator(generator_list_log))
